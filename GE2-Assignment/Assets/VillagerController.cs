6041356cc5cf3c48a74f03049c3b927312815e90
Yasir Çeltik <55563251+omikronik@users.noreply.github.com>
2023-05-04 13:58:38 +0100

    mostly done village, work on villeg center start

--- GE2-Assignment/Assets/VillagerController.cs
+++ GE2-Assignment/Assets/VillagerController.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class VillagerController : MonoBehaviour
 {
+    float collectionTimer = 3.0f;
+    public bool hasBerry = false;
+
+    void OnDrawGizmos()
+    {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        if (collider != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(collider.center, collider.size);
+        }
+    }
+
 
     public void OnTriggerEnter(Collider other)
     {
+        Debug.Log("collided berry");
         if (other.tag == "berry_bush")
         {
+            Debug.Log("collided berry");
             GetComponent<StateMachine>().ChangeState(new GoToBerryState());
         }
     }
 
+    IEnumerator CollectionTimer()
+    {
+        yield return new WaitForSeconds(collectionTimer);
+        GetComponent<StateMachine>().ChangeState(new GoToBaseState());
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<StateMachine>().ChangeState(new ScavengeState());
         GetComponent<StateMachine>().SetGlobalState(new Alive());
+
+        Debug.Log("hello");
 
     }
 
